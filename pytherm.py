a554ecc27160c9f925eebd08884b5f4ec181daa1
Adrien Vilquin Barrajon <avilqu@gmail.com>
2021-08-05 02:56:04 +1200

    added code

--- pytherm.py
+++ pytherm.py
@@ -1,3 +1,12 @@
 ''' Reads temperature from a DS18B20 sensor. Written for Raspberry Pi 4B.
     @author: Adrien Vilquin Barrajon <avilqu@gmail.com>
 '''
+
+import time
+from w1thermsensor import W1ThermSensor
+
+sensor = W1ThermSensor()
+while True:
+    temp = sensor.get_temperature()
+    print('Temperature (C): ' + temp)
+    time.sleep(1)

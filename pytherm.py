d4574d6508baacfbd771841662c64e6b154f787a
Adri Vilba <avilqu@gmail.com>
2021-08-05 19:27:40 +1200

    new code without w1thermsensor library

--- pytherm.py
+++ pytherm.py
@@ -4,19 +4,52 @@
     @author: Adrien Vilquin Barrajon <avilqu@gmail.com>
 '''
 
-import time
-from w1thermsensor import W1ThermSensor, Sensor
-
+# import time
+# from w1thermsensor import W1ThermSensor, Sensor
 # for sensor in W1ThermSensor.get_available_sensors():
 #     print("Sensor %s has temperature %.2f" %
 #           (sensor.id, sensor.get_temperature()))
+# local_sensor = W1ThermSensor(Sensor.DS18B20, '3c01d0752026')
+# remote_sensor = W1ThermSensor(Sensor.DS18B20, '3c01d075bfb0')
+# while True:
+#     local_temp = local_sensor.get_temperature()
+#     remote_temp = remote_sensor.get_temperature()
+#     print('Local (C): ' + str(local_temp))
+#     print('Remote (C): ' + str(remote_temp))
+#     time.sleep(1)
 
-local_sensor = W1ThermSensor(Sensor.DS18B20, '3c01d0752026')
-remote_sensor = W1ThermSensor(Sensor.DS18B20, '3c01d075bfb0')
+import os
+import glob
+import time
+
+# os.system('modprobe w1-gpio')
+# os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
 
 while True:
-    local_temp = local_sensor.get_temperature()
-    remote_temp = remote_sensor.get_temperature()
-    print('Local (C): ' + str(local_temp))
-    print('Remote (C): ' + str(remote_temp))
+    print(read_temp())
     time.sleep(1)

2cb70516b4ab0f9e863f22c2b4166a74cb51f2b8
Adri Vilba <avilqu@gmail.com>
2021-08-08 01:30:26 +1200

    dev

--- DS18B20.py
+++ DS18B20.py
@@ -39,30 +39,33 @@ class DS18B20:
 
 
 def record_sensors(interval):
-    if not os.path.exists(cfg.DATA_DIR):
-        os.mkdir(cfg.DATA_DIR)
-
-    start_date = datetime.now().strftime('%Y-%m-%d')
-    data_filename = cfg.DATA_DIR + start_date + '.txt'
-
-    if os.path.exists(data_filename):
-        f = open(data_filename, 'a')
-    else:
-        f = open(data_filename, 'w')
-
     while True:
-        if start_date != datetime.now().strftime('%Y-%m-%d'):
-            print('End of day...')
-            break
-        data_string = datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + ','
-        for sensor in sensors:
-            data_string = data_string + str(sensor.read_temp()) + ','
-        print(data_string)
-        data_string = data_string + '\n'
-        f.write(data_string)
-        time.sleep(interval)
+        if not os.path.exists(cfg.DATA_DIR):
+            os.mkdir(cfg.DATA_DIR)
 
-    f.close()
+        start_date = datetime.now().strftime('%Y-%m-%d')
+        data_filename = cfg.DATA_DIR + start_date + '.txt'
+
+        print('Writing data to', data_filename)
+
+        if os.path.exists(data_filename):
+            f = open(data_filename, 'a')
+        else:
+            f = open(data_filename, 'w')
+
+        while True:
+            if start_date != datetime.now().strftime('%Y-%m-%d'):
+                print('End of day...')
+                break
+            data_string = datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + ','
+            for sensor in sensors:
+                data_string = data_string + str(sensor.read_temp()) + ','
+            print(data_string)
+            data_string = data_string + '\n'
+            f.write(data_string)
+            time.sleep(interval)
+
+        f.close()
 
 
 sensors_dirs = glob.glob(cfg.BASE_DIR + '28*')

4b35214fcb73ba8270ce91e9ca10a62730ee9edf
Adri Vilba <avilqu@gmail.com>
2021-08-28 23:18:32 +1200

    dev

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
     @author: Adrien Vilquin Barrajon <avilqu@gmail.com>
 '''
 
-BASE_DIR = '/sys/bus/w1/devices/'       # System one-wire directory
-DATA_DIR = '/home/pi/pytherm/data/'     # Data storage folder
-RECORD_INTERVAL = 60                    # Record interval (seconds)
+BASE_DIR = '/sys/bus/w1/devices/'         # System one-wire directory
+# DATA_DIR = '/home/pi/pytherm/data/'       # Data storage folder
+DATA_DIR = '/home/tan/dev/pytherm/data/'  # Data storage folder
+RECORD_INTERVAL = 60                      # Record interval (seconds)
--- temp_plot.py
+++ temp_plot.py
@@ -1,11 +1,32 @@
+#!/usr/bin/python3
+
+''' Generate temperature plot with data in CSV file.
+    @author: Adrien Vilquin Barrajon <avilqu@gmail.com>
+'''
+
 import numpy as np
 import csv
+import seaborn as plt
 
-with open('/home/tan/dev/2021-08-09.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        print(
-            f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+import config as cfg
+
+
+def generate_plot(date):
+    x = []
+    y1 = []
+    y2 = []
+
+    with open(f'{cfg.DATA_DIR}{date}.txt') as csv_file:
+
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        data_count = 0
+        for row in csv_reader:
+            x.append(row[0])
+            y1.append(row[1])
+            y2.append(row[2])
+            data_count += 1
+
+    plt.plot(x, y1, color='red', label='sensor 1')
+    plt.plot(x, y2, color='blue', label='sensor 2')
+    plt.title('Pyplot playground')
+    plt.legend(loc='upper right')

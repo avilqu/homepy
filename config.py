7cac740ec6d4b7358b6d624d9adeac1351877d9c
Adri Vilba <avilqu@gmail.com>
2022-03-29 21:26:46 +1300

    added sqlite3 support

--- config.py
+++ config.py
@@ -5,3 +5,11 @@
 BASE_DIR = '/sys/bus/w1/devices/'         # System one-wire directory
 DATA_DIR = '/home/pi/pytherm/data/'       # Data storage folder
 RECORD_INTERVAL = 60                      # Record interval (seconds)
+DB_FILE = 'homepy.db'
+SQL_INIT = '''
+    CREATE TABLE IF NOT EXISTS temp (
+        id integer PRIMARY KEY,
+        timestamp text,
+        sensor1 real,
+        sensor2 real
+    );'''
--- sensors.py
+++ sensors.py
@@ -9,12 +9,53 @@ import glob
 from datetime import datetime
 import time
 import sqlite3
+from sqlite3 import Error
 
 import config as cfg
 import DS18B20
 
+db = None
+try:
+    db = sqlite3.connect(cfg.DB_FILE)
+except Error as e:
+    print(e)
+
+sensors_dirs = glob.glob(cfg.BASE_DIR + '28*')
+sensors = []
+for item in sensors_dirs:
+    sensors.append(DS18B20(item))
+if not sensors:
+    print('No sensor found on host system.')
+else:
+    print(sensors)
+
+if db is not None:
+    try:
+        c = db.cursor()
+        c.execute(cfg.SQL_INIT)
+    except Error as e:
+        print(e)
+
+
+def db_record_temp(data):
+    sql = ''' 
+        INSERT INTO temp(timestamp,sensor1,sensor2)
+        VALUES(?,?,?) '''
+    cur = db.cursor()
+    cur.execute(sql, data)
+    db.commit()
+    return cur.lastrowid
+
 
 def record_sensors():
+    while True:
+        db_record_temp((datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+                        sensors[0].read_temp(),
+                        sensors[1].read_temp()))
+        time.sleep(cfg.RECORD_INTERVAL)
+
+
+def record_sensors_to_file():
     while True:
         if not os.path.exists(cfg.DATA_DIR):
             os.mkdir(cfg.DATA_DIR)
@@ -43,23 +84,18 @@ def record_sensors():
             time.sleep(cfg.RECORD_INTERVAL)
 
 
-sensors_dirs = glob.glob(cfg.BASE_DIR + '28*')
-if not sensors_dirs:
-    print('No sensor found on host system.')
-sensors = []
-for item in sensors_dirs:
-    sensors.append(DS18B20(item))
-
 if __name__ == "__main__":
 
     import argparse
 
     parser = argparse.ArgumentParser(
-        description='Reads output from DS18B20 temperature sensors.')
+        description='Reads output from DS18B20 temperature sensors. Check config.py for settings.')
     parser.add_argument(
         '-l', '--loop', help='print temp loop (1s interval)', action='store_true')
     parser.add_argument(
-        '-r', '--record', help='record data to file', action='store_true')
+        '-r', '--record', help='record data to database', action='store_true')
+    parser.add_argument(
+        '-f', '--file', help='record data to file', action='store_true')
 
     args = parser.parse_args()
 
@@ -71,6 +107,9 @@ if __name__ == "__main__":
             for sensor in sensors:
                 print(sensor.sensor_id, ':', sensor.read_temp())
             time.sleep(1)
+
+    elif args.file:
+        record_sensors_to_file()
 
     elif args.record:
         record_sensors()

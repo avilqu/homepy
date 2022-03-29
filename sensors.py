bdc37f582d8d849c2e52beff08f8629d7eef405c
Adri Vilba <avilqu@gmail.com>
2022-03-29 23:48:07 +1300

    fixed db calls

--- sensors.py
+++ sensors.py
@@ -14,12 +14,6 @@ from sqlite3 import Error
 import config as cfg
 from DS18B20 import *
 
-db = None
-try:
-    db = sqlite3.connect(cfg.DB_FILE)
-except Error as e:
-    print(e)
-
 sensors_dirs = glob.glob(cfg.BASE_DIR + '28*')
 sensors = []
 for item in sensors_dirs:
@@ -29,15 +23,17 @@ if not sensors:
 else:
     print(sensors)
 
-if db is not None:
+
+def db_connect():
+    db = None
     try:
-        c = db.cursor()
-        c.execute(cfg.SQL_INIT)
+        db = sqlite3.connect(cfg.DB_FILE)
     except Error as e:
         print(e)
+    return db
 
 
-def db_write_temp(data):
+def db_write_temp(db, data):
     sql = ''' 
         INSERT INTO temp(timestamp,sensor1,sensor2)
         VALUES(?,?,?) '''
@@ -49,20 +45,20 @@ def db_write_temp(data):
 
 def record_sensors():
     while True:
-        db_write_temp((datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-                       sensors[0].read_temp(),
-                       sensors[1].read_temp()))
+        db_write_temp(db_connect(), (datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+                                     sensors[0].read_temp(),
+                                     sensors[1].read_temp()))
         time.sleep(cfg.RECORD_INTERVAL)
 
 
-def read_last():
+def read_last(db):
     sql = 'SELECT * FROM temp LIMIT 1'
     cur = db.cursor()
     cur.execute(sql)
     return cur.fetchall()[0]
 
 
-def read_last_24h():
+def read_last_24h(db):
     sql = 'SELECT * FROM temp LIMIT 1440'
     cur = db.cursor()
     cur.execute(sql)
@@ -97,7 +93,7 @@ if __name__ == "__main__":
         record_sensors()
 
     elif args.show:
-        print(read_last_24h())
+        print(read_last_24h(db_connect()))
 
     else:
         for sensor in sensors:

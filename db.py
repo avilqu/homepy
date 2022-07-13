54c340c2601451079eadee11b743fc4d5b40e53e
Adri Vilba <avilqu@gmail.com>
2022-07-13 15:37:22 +1200

    adding DB class def

---
+++ db.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+''' Contains all methods to interact with InfluxDB.
+    @author: Adrien Vilquin Barrajon <avilqu@gmail.com>
+'''
+
+from influxdb import InfluxDBClient
+
+import config as cfg
+
+
+class DB:
+
+    def __init__(self):
+        self.db = InfluxDBClient(host=cfg.DB_URL, port=cfg.DB_PORT)
+        self.db.switch_database(cfg.DB_NAME)
+
+    def write_data_points(self, data_points):
+        self.db.write_points(data_points)
